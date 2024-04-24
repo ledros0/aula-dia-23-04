3df0a95d6fc3abfcdcee3e06f65f33be555ce2e1
MATEUS BORBA SILVEIRA <mateus.bsilveira@senacsp.edu.br>
2024-04-23 22:07:37 -0300

    aula 23 04

---
+++ soma da calculadora merd.js
@@ -0,0 +1,9 @@
+function soma(){
+
+    let numero1 = parseFloat(document.getElementById("numero1").value);
+    let numero2 = parseFloat(document.getElementById("numero2").value);
+    let numero3 = parseFloat(document.getElementById("numero3").value);
+
+    alert(numero1 + numero2 + numero3);
+
+}
